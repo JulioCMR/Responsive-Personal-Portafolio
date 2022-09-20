4117aef2aa01af9f5c652e9203c75fe6c7d27b33
JulioCMR <80418425+JulioCMR@users.noreply.github.com>
2022-09-19 19:00:26 -0500

    Added Skills section and started Qualifications section

--- js/main.js
+++ js/main.js
@@ -27,5 +27,26 @@ function linkAction() {
 
 navLink.forEach(n => n.addEventListener("click", linkAction))
 
+// Skills lists -----
+const skillsContent = document.querySelectorAll(".skills-content"),
+      skillsHeader = document.querySelectorAll(".skills-header")
+
+
+
+function toggleSkills() {
+
+    let itemClass = this.parentNode.className
+
+
+    for (let i = 0; i < skillsContent.length; i++) {
+        skillsContent[i].className = "skills-content skills-close"
+    }
+
+    if(itemClass === "skills-content skills-close"){
+        this.parentNode.className = "skills-content skills-open"
+    }
+}
+
+skillsHeader.forEach(el => el.addEventListener("click", toggleSkills) )
 
 
