d333e3784319d5fde40d43649d458e69daf2db0f
JulioCMR <80418425+JulioCMR@users.noreply.github.com>
2022-09-28 23:11:04 -0500

    Added Qualification section

--- js/main.js
+++ js/main.js
@@ -50,3 +50,28 @@ function toggleSkills() {
 skillsHeader.forEach(el => el.addEventListener("click", toggleSkills) )
 
 
+// Qualifications tabs -----
+
+const tabs = document.querySelectorAll("[data-target]"),
+      tabContents = document.querySelectorAll("[data-content]")
+
+      
+
+tabs.forEach(tab => {
+
+    tab.addEventListener("click", () => {
+        const target = document.querySelector(tab.dataset.target)
+
+        tabContents.forEach(tabContent => {
+            tabContent.classList.remove("qualification-active")
+        })
+        target.classList.add("qualification-active")
+
+        // This piece of code shows an error.
+        /* tab.forEach(tab => {
+            tab.classList.remove("qualification-active")
+        })
+        tab.classList.add("qualification-active") */
+        
+    })
+})
